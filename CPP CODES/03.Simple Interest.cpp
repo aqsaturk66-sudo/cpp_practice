8032bcd24c780edf91d9dc70f06d7b933fd4e7d3
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-09 14:31:53 +0500

    Update 03.Simple Interest.cpp

--- CPP CODES/03.Simple Interest.cpp
+++ CPP CODES/03.Simple Interest.cpp
@@ -1,1 +1,12 @@
-
+//simple interest= SI=P*R*T/100
+//P=principal (original money), R=rate of interest per year, T=time (in year)
+#include<iostream>
+using namespace std;
+int main(){
+  float P=5000;
+float R= 10;
+float T= 1;
+float SI=(P*R*T)/100;
+cout<<SI;
+return 0;
+}
