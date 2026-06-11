ef6c091edc5035c680ac6469976df17c22ccf944
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-11 16:13:21 +0500

    Update 05.user input square of a num.cpp

--- CPP CODES/05.user input square of a num.cpp
+++ CPP CODES/05.user input square of a num.cpp
@@ -1,1 +1,11 @@
+//Taking input in C++ means getting data from the user and storing it in a variable using cin.
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cin>>x; //The >> operator is called the extraction operator because it extracts data from the keyboard
+input and stores it in a variable.
+cout<<x*x;
+return 0;
+}
 
