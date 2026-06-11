ddbe5d5ec405831a1a4c24889efc8c45094d63d0
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-11 16:31:46 +0500

    Update and rename 06.sum of two num using inputs.cpp to 06.sum of two num using inputs from user.cpp

---
+++ CPP CODES/06.sum of two num using inputs from user.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x;
+  cout<<"Enter first num :";
+  cin>>x;
+  int y;
+  cout<<"Enter second num :";
+cin>>y;
+cout<<"Sum is :" << x+y;
+return 0;
+}
