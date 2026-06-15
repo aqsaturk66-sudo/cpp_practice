03eebf3a4988b14995dc22bf1915aca734bbbd40
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-15 22:49:42 +0500

    Update 07.modulus Operator.cpp

--- CPP CODES/07.modulus Operator.cpp
+++ CPP CODES/07.modulus Operator.cpp
@@ -1,6 +1,15 @@
 // % is used for modulus
 // modulus means reminder when we divide a one num by other
+// a%b = a if [a<b] , 2%8 = 2
+// a%a = 0
+//a%b = 0 (when a is divisible by b or b is factor of a)
+// a%(-b)= a%b
+//(-a)%b= -[a%b]
 #include<iostream>
 using namespace std;
 int main(){
-  int x;
+  int x = 8;
+int y =3; 
+cout<<8%3; //when we divide 8 by 3 we get reminder 2 
+return 0;
+}
