56bd87b8c1c96c178efc1f9214a1c45ebf724393
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-23 15:10:57 +0500

    Update 11.float.cpp

--- CPP CODES/11.float.cpp
+++ CPP CODES/11.float.cpp
@@ -1,1 +1,9 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+  float x=5.0/2;
+cout<<x;
+return 0;
+}
+// when we use float x=5/2 it will still give integer in output because computer get 5 and 2 seperatly and take them as integers so in the end gives the integer in output
+even though we used float but untill we use float x= 5.0/2 or 5/2.0 or 5.0/2.0 we can't get float in result
