19bb5053f765d673c3a05a6ae9705cf8d808cf8b
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-23 14:55:50 +0500

    Update 10.char data type.cpp

--- CPP CODES/10.char data type.cpp
+++ CPP CODES/10.char data type.cpp
@@ -1,1 +1,36 @@
+//character data type is a data type that stores a single character such as a letter, digit or symbol. 
+char is written inside the single inverted comas
+
+#include<iostream>
+using namespace std;
+int main(){
+char x;
+x = '1';
+cout<<x;
+return 0;
+}
+//ASCII value= 
+#include<iostream>
+using namespace std;
+int main(){
+  char ch;
+cin>> ch;
+cout<< (int) ch; // ch to int ASCII VALUE
+return 0;
+}
+// int to char 
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cin>> x;
+cout<<(char) x;
+  // also we can write in this format 
+  //cin>> x;
+  // char ch = (char) x;
+  // cout<< ch;
+return 0;
+}
+
+
 
