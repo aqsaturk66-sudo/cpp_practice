155782ff7dd78238672016d662e46897bd9f3357
Aqsa Qadir <aqsaturk66@gmail.com>
2026-06-30 15:21:56 +0500

    Update 13.Absolute value.cpp

--- 13.Absolute value.cpp
+++ 13.Absolute value.cpp
@@ -1,1 +1,17 @@
-
+// + - % / * are arithmetic operator
+// ==, !=, >, <=, >= are relational operator
+#include<iostream>
+using namespace std;
+int main(){
+  int x;
+cout<<"enter x";
+cin>>x;
+if(x>=0){
+cout<<x;
+}
+else{
+cout<<-x;
+return 0;
+}
+}
+//
